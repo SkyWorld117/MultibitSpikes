43771397b68c7082d08235e09c7800009c75d880
zyi <zyi@sbrinz1.cm.cluster>
2025-01-10 12:26:14 +0100

    Remove question mark

--- test.py
+++ test.py
@@ -82,7 +82,7 @@ else:
 # trainer.main(args)
 
 """
-Used for data generation:
+Standard:
 python -m test --N 8 --R 10 --T 10 --acc 0.80 --model FashionMNISTNet --data-path /scratch/zyi/codeSpace/data --dataset FashionMNIST --batch-size 128 --opt adam --lr 2e-3 --lr-scheduler none --epochs 5 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes --mixup-alpha 0.0 --cutmix-alpha 0.0 --label-smoothing 0.0 --disable-amp
 python -m test --N 8 --R 10 --T 10 --acc 0.80 --model MNISTNet --data-path /scratch/zyi/codeSpace/data --dataset MNIST --batch-size 128 --opt adam --lr 2e-3 --lr-scheduler none --epochs 5 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes --mixup-alpha 0.0 --cutmix-alpha 0.0 --label-smoothing 0.0 --disable-amp
 python -m test --N 8 --R 10 --T 10 --acc 0.80 --model NMNISTNet --data-path /scratch/zyi/codeSpace/data --dataset NMNIST --batch-size 128 --opt adam --lr 2e-3 --lr-scheduler none --epochs 5 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes --mixup-alpha 0.0 --cutmix-alpha 0.0 --label-smoothing 0.0 --disable-amp
@@ -101,7 +101,7 @@ python -m test --N 2 --R 10 --T 10 10 --acc 0.80 --model FashionMNISTNet --data-
 python -m test --N 2 --R 10 --T 10 10 --acc 0.80 --model MNISTNet --data-path /scratch/zyi/codeSpace/data --dataset MNIST --batch-size 128 --opt adam --lr 2e-3 --lr-scheduler none --epochs 50 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate --mixup-alpha 0.0 --cutmix-alpha 0.0 --label-smoothing 0.0 --disable-amp
 python -m test --N 2 --R 10 --T 10 10 --acc 0.80 --model NMNISTNet --data-path /scratch/zyi/codeSpace/data --dataset NMNIST --batch-size 128 --opt adam --lr 2e-3 --lr-scheduler none --epochs 50 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate --mixup-alpha 0.0 --cutmix-alpha 0.0 --label-smoothing 0.0 --disable-amp
 python -m test --N 2 --R 10 --T 10 10 --acc 0.80 --model DVSGestureNet --data-path /scratch/zyi/codeSpace/data --dataset DVSGesture --batch-size 128 --opt adam --lr 1e-3 --lr-scheduler cosa --epochs 200 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate
-?python -m test --N 2 --R 5 --T 10 10 --acc 0.80 --model CIFAR10Net --data-path /scratch/zyi/codeSpace/data --dataset CIFAR10 --batch-size 128 --opt adam --lr 1e-5 --lr-scheduler none --epochs 1000 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate
+python -m test --N 2 --R 5 --T 10 10 --acc 0.80 --model CIFAR10Net --data-path /scratch/zyi/codeSpace/data --dataset CIFAR10 --batch-size 128 --opt adam --lr 1e-5 --lr-scheduler none --epochs 1000 --lr-warmup-epochs 0 --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate
 
 Plots:
 python -m test --N 2 --R 10 --T 10 10 --dataset FashionMNIST --output-dir /scratch/zyi/codeSpace/MultibitSpikes/firerate --plot-from-data
